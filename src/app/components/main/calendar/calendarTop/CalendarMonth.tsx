e601cf22234211134d60e857193aebdd292d1dd5
Hayeon-Jung <jhy00714@gmail.com>
2024-03-16 18:21:28 +0900

    달력 년/월 출력

---
+++ src/app/components/main/calendar/calendarTop/CalendarMonth.tsx
@@ -0,0 +1,16 @@
+import { useRecoilState } from "recoil"
+import { monthAtom, yearAtom } from "../atom";
+import { useEffect, useState } from "react";
+
+const CalendarMonth = () => {
+  const [year, setYear] = useRecoilState(yearAtom);
+  const [month, setMonth] = useRecoilState(monthAtom);
+
+  return (
+    <>
+      <div>{year} {month + 1}</div>
+    </>
+  )
+}
+
+export default CalendarMonth
