a012a29613226d69f9626b25897dae719ec895f2
Hayeon-Jung <jhy00714@gmail.com>
2024-03-25 22:32:50 +0900

    메인화면 출력

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Main from "./components/main/Main";
+'use client';
+
+import Image from 'next/image';
+import styles from './page.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  // 렌더링이 될 때 만약 로그인이 되어있다면 routing for main  
+  // 렌더링이 될 때 만약 로그인이 되어있다면 routing for main
+  const router = useRouter();
 
   return (
     <main>
-      <Main />
+      <button onClick={() => router.push('/join')}>시작하기</button>
     </main>
   );
 }
