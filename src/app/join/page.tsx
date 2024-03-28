db3485adaa0a07b30624aac53843b08aeb62bc98
Hayeon-Jung <jhy000714@gmail.com>
2024-03-28 13:06:30 +0900

    로그인 및 회원가입 폼 완성

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -2,13 +2,58 @@
 
 import { useForm } from 'react-hook-form';
 
+interface userData {
+  userId: string;
+  userPwd: string;
+}
+
 const SignUp = () => {
-  const { handleSubmit, getValues, register } = useForm();
+  const {
+    handleSubmit,
+    getValues,
+    register,
+    formState: { errors },
+  } = useForm<userData>();
+
+  const onSubmitForm = () => {
+    const data = {
+      userId: getValues('userId'),
+      userPwd: getValues('userPwd'),
+    };
+    console.log(data);
+  };
 
   return (
     <>
-      <form>
-        <div></div>
+      <form onSubmit={handleSubmit(onSubmitForm)}>
+        <div>아이디</div>
+        <input
+          {...register('userId', {
+            required: {
+              message: '아이디를 입력해주세요.',
+              value: true,
+            },
+            pattern: {
+              message: '정확한 아이디를 입력해주세요.',
+              value: /[A-Za-z0-9!@#$%^&*-_+]/,
+            },
+          })}
+          type="text"
+        />
+        <div>{errors.userId?.message}</div>
+        <div>비밀번호</div>
+        <input
+          {...register('userPwd', {
+            required: {
+              message: '비밀번호를 입려해주세요.',
+              value: true,
+            },
+            pattern: /[A-Za-z0-9!@#$%^&*-_+]/,
+          })}
+          type="password"
+        />
+        <div>{errors.userPwd?.message}</div>
+        <button>회원가입</button>
       </form>
     </>
   );
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
 interface userData {
@@ -9,7 +10,14 @@ interface userData {
 }
 
 const SignIn = () => {
-  const { handleSubmit, getValues, register } = useForm();
+  const {
+    handleSubmit,
+    getValues,
+    register,
+    formState: { errors },
+  } = useForm<userData>();
+
+  const router = useRouter();
 
   const mutation = useMutation({
     mutationFn: (userData: userData) => {
@@ -18,18 +26,49 @@ const SignIn = () => {
         body: JSON.stringify(userData),
       });
     },
-    onSuccess: () => {},
+    onSuccess: () => {
+      router.push('/diary');
+    },
   });
+
+  const handleLogin = async (ele: userData) => {
+    mutation.mutate({
+      userId: ele.userId,
+      userPwd: ele.userPwd,
+    });
+  };
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(handleLogin)}>
         <div>아이디</div>
-        <input />
+        <input
+          {...register('userId', {
+            required: {
+              message: '아이디를 입력해주세요.',
+              value: true,
+            },
+            pattern: {
+              message: '정확한 아이디를 입력해주세요.',
+              value: /[A-Za-z0-9!@#$%^&*-_+]/,
+            },
+          })}
+        />
         <div>비밀번호</div>
-        <input />
+        <input
+          {...register('userPwd', {
+            required: {
+              message: '비밀번호를 입려해주세요.',
+              value: true,
+            },
+            pattern: /[A-Za-z0-9!@#$%^&*-_+]/,
+          })}
+          type="password"
+        />
+        <br />
+        <button>로그인</button>
       </form>
-      <div>회원가입</div>
+      <div onClick={() => router.push('/join')}>회원가입</div>
     </>
   );
 };
