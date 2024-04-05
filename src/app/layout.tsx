20bab92724eaf5a548120c6a28b9511a2834f5e9
Hayeon-Jung <jhy000714@gmail.com>
2024-04-05 15:36:38 +0900

    통신을 위한 head 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import ReactQueryProviders from './hooks/useReactQuery';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className={inter.className}>
         <ReactQueryProviders>{children}</ReactQueryProviders>
       </body>
